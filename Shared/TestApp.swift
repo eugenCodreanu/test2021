45406e8fc19f288d0201c889d051dcc4c1ba3944
Codreanu Eugen <condreanu@Codreanus-Mac-mini.local>
2021-02-08 10:18:00 +0200

    first comit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Codreanu Eugen on 08.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
